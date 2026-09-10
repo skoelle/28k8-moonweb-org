58ce567c865a0947a814d1722e4f1f9419e2c3f1
Stefan Koelle <stefan.koelle@gmail.com>
2026-09-10 21:40:29 +0200

    trailing slash fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 export default defineConfig({
   site: 'https://28k8.moonweb.org',
+  trailingSlash: 'never',
   integrations: [react()],
   server: { host: '0.0.0.0' },
   vite: { server: { allowedHosts: true } }
